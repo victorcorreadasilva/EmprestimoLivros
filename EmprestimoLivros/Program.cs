ac07854deb8bc534083e2ef2002eb9798590e34e
Victor Corrêa <161853474+victorcorreadasilva@users.noreply.github.com>
2025-10-20 09:44:30 -0300

    Add files via upload

---
+++ EmprestimoLivros/Program.cs
@@ -0,0 +1,48 @@
+using EmprestimoLivros.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                // Sql Server -> 
+                //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+                // Sql Lite -> 
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
